38874a97d15c8c489cc5815afbf5a50e635130cf
nettlesoup <sanettleingham@hotmail.co.uk>
2020-06-04 12:39:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there" });
+  res.send({
+    hi: "there",
+    bye: "buddy",
+  });
 });
 
 const PORT = process.env.PORT || 5000;
