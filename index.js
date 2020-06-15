4c196893796208e44a1b004dc422ba338babbddd
nettlesoup <sanettleingham@hotmail.co.uk>
2020-06-15 16:26:44 +0100

    Refactor into subfolders

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("./services/passport");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({
-    hi: "there",
-    bye: "buddy",
-  });
-});
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
