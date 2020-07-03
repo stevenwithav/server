23e95dbd4d4650b87b49ec43a85df2d091db127f
nettlesoup <sanettleingham@hotmail.co.uk>
2020-07-03 15:20:02 +0100

    Add ability to add credits via stripe

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 
 const keys = require("./config/keys");
 
@@ -15,6 +16,9 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+// Add body-parser middleware
+app.use(bodyParser.json());
+
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
@@ -26,6 +30,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
