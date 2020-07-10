fa64273320902e4035d4d8cda2ea119755e76c6f
nettlesoup <sanettleingham@hotmail.co.uk>
2020-07-10 10:57:22 +0100

    Connect App to Sendgrid for emails

---
+++ services/emailTemplates/surveyTemplate.js
@@ -0,0 +1,17 @@
+module.exports = (survey) => {
+  return `
+    <html>
+      <body>
+        <div style="text-align: center;">
+          <h3>I'd like your feedback!</h3>
+          <p>Please anser the following questions:</p>
+          <p>${survey.body}</p>
+          <div>
+            <a href="http://localhost:3000">Yes</a>
+            <a href="http://localhost:3000">No</a>
+          </div>
+        </div>
+      </body>
+    </html>
+  `;
+};
